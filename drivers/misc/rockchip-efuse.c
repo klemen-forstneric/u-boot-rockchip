51e7ba784740f6bbc25811e630b3aa23d092dc65
Klemen <klemen.forstneric@gmail.com>
2019-05-10 12:33:39 +0200

    Changed to reading from 4

--- drivers/misc/rockchip-efuse.c
+++ drivers/misc/rockchip-efuse.c
@@ -57,7 +57,7 @@ static int read_efuses(cmd_tbl_t *cmdtp, int flag, int argc,
     return 0;
   }
 
-  ret = misc_read(dev, 0, &fuses, sizeof(fuses));
+  ret = misc_read(dev, 4, &fuses, sizeof(fuses));
   if (ret) {
     printf("%s: misc_read failed\n", __func__);
     return 0;
